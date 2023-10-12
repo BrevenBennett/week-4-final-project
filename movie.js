0ddcf8522c6385d5ec55e716b9171a610ad2fd61
BrevenBennett <Breven.stars@gmail.com>
2023-10-12 11:39:58 -0500

    working on filtering the year

--- movie.js
+++ movie.js
@@ -1,62 +1,57 @@
 const moviesListEl = document.querySelector(".movie-list");
 const searchTitleEl = document.querySelector(".header__movie--search");
-const loadingSpinEl = document.querySelector(".movie__loading");
-const sortEl = document.querySelector(".year__select");
-const id = localStorage.getItem("id");
 
 async function renderMovies(filter) {
-  setTimeout(() => {
-    loadingSpinEl.classList += " movie__loading--spinner";
-  }, 500);
-
-  moviesListEl.style.visibility = "hidden";
-
+  searchTitleEl.innerHTML += ` ${title}`;
   const movies = await fetch(
     `http://www.omdbapi.com/?apikey=53de5b3d&s=${title}`
   );
+
   const moviesData = await movies.json();
 
-  const moviesArray = moviesData.Search.slice(0, 6);
+  const moviesArray = moviesData.Search;
 
-  if (filter === "Oldest_To_Newest") {
-    moviesArray.sort((a, b) => b.Year - a.Year);
-  } else if (filter === "Newest_To_Oldest") {
+ if (filter === "Oldest_To_Newest") {
     moviesArray.sort((a, b) => a.Year - b.Year);
+  } else if (filter === "Newest_To_Oldest") {
+    moviesArray.sort((a, b) => b.Year - a.Year);
   }
 
-  setTimeout(() => {
-    moviesListEl.style.visibility = "visible";
-    moviesListEl.innerHTML = moviesArray
-      .map((movie) => movieHTML(movie))
-      .join("");
-    searchTitleEl.innerHTML += ` ${title}`;
-    loadingSpinEl.classList.remove("movie__loading");
-  }, 1200);
+  moviesListEl.innerHTML = moviesArray
+    .map((movie) => movieHTML(movie))
+    .slice(0, 6)
+    .join("");
+}
+
+function filterYear(event) {
+  console.log(event.target.value)
+  renderMovies(event.target.value);
 }
 
 function onSearchChange(event) {
   title = event.target.value;
   event.preventDefault();
-  renderMovies()
+  renderMovies();
 }
 
 function movieHTML(movie) {
   return `<div class="movie">
     <figure class="movie__img--wrapper">
-    <img src="${movie.Poster}" alt="" class="movie__img" />
+      <img src="${movie.Poster}" alt="" class="movie__img" />
     </figure>
     <h4 class="movie__title">${movie.Title}</h4>
-    <h4 class="movie__year">${movie.Year}</h4>`;
+    <h4 class="movie__year">${movie.Year}</h4>
+    </div>`;
 }
 
-function sortMovieYear(event) {
-  renderMovies(event.target.value);
-}
+// function sortMovieYear(event) {
+//   renderMovies(event.target.value);
+// }
 
-function openMenu() {
-  document.body.classList += " menu--open";
-}
+// function openMenu() {
+//   document.body.classList += " menu--open";
+// }
 
-function closeMenu() {
-  document.body.classList.remove("menu--open");
-}
+// function closeMenu() {
+//   document.body.classList.remove("menu--open");
+// }
